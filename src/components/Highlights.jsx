d56f8dbd787b412285f4d4b49f481a33fb6c3291
Minh Le <minh.le@ledminh.dev>
2024-06-18 06:35:53 -0400

    VideoCarousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React from "react";
 import { watchImg, rightImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -13,6 +14,8 @@ const Highlights = () => {
     gsap.to(".link", {
       opacity: 1,
       y: 0,
+      duration: 1,
+      stagger: 0.25,
     });
   }, []);
 
@@ -22,7 +25,7 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end justify-between">
+        <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
@@ -37,6 +40,8 @@ const Highlights = () => {
             </p>
           </div>
         </div>
+
+        <VideoCarousel />
       </div>
     </section>
   );
